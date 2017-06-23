7f8cd9a2f275c464fd10400283a0e7dbf5697bdc
Kononov Anton <kononov@elewise.com>
2017-06-23 09:26:03 +0400

    Version update

--- DanTup.BrowserSelector/Properties/AssemblyInfo.cs
+++ DanTup.BrowserSelector/Properties/AssemblyInfo.cs
@@ -3,5 +3,7 @@
 [assembly: AssemblyTitle("DanTup.BrowserSelector")]
 [assembly: AssemblyProduct("DanTup.BrowserSelector")]
 [assembly: AssemblyCopyright("Copyright © Danny Tuppeny 2015")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.30.0.0")]
+[assembly: AssemblyFileVersion("0.30.0.0")]
+[assembly: AssemblyDescription("modify by StGolem")]
+
